07a5abe8f0dae4dcbbd7a5695b22d5e7c5681c51
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2025-07-29 20:39:07 -0300

    💄 (style): banner css

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,7 +1,10 @@
+import './Banner.css';
 
 function Banner() {
     return (
+        <header className="banner">
             <img src="/imagens/banner.png" alt="O banner da página principal"/>
+        </header>
     )
 }
 
